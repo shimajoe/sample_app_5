fbbf66359d8603420f2b9a29c87e7ffe47911b46
Ubuntu <ubuntu@ip-172-31-49-45.ec2.internal>
2023-02-16 02:21:10 +0000

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
@@ -14,4 +18,6 @@ Rails.application.routes.draw do
   resources :users
   # account_activations resourceのeditへのルーティングのみを生成
   resources :account_activations, only: [:edit]
+  # password_resets resourceのnew,create,edit,updateへのルーティングのみを生成
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
